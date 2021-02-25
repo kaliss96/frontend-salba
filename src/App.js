62eb1bf5d60facf50ca83b8d55ae393210eafe65
Karen Aguirre <aguirretellez96@gmail.com>
2021-02-24 20:23:47 -0600

    Lanzamiento

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
 import Dashboard from './components/Dashboard';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -17,7 +17,6 @@ function App() {
         </Switch>
       </BrowserRouter>
       <Footer />
-      
     </div>
   );
 }
